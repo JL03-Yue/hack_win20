3183d86fd4a24cc3699c76d764c65fc83e229eb3
Annie Li <liqy112008@gmail.com>
2021-02-04 10:56:39 +0800

    commit on 'master'

--- App.js
+++ App.js
@@ -13,18 +13,7 @@ import Minepage from './screens/mine/minepage'
 import Marketpage from './screens/market/marketpage'
 import {createStackNavigator} from 'react-navigation-stack'
 
-function WildernessExplorer(){
-  return(
-    <View>
-      <Text>WildernessExplorer</Text>
-      <Button
-        title="Go to profile screen"
-      />
 
-    </View>
-    
-  );
-}
 
 export default function App()
 {
